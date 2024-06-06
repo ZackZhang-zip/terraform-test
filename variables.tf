ffbc64337ebd394768294a213d76cb5d7e4eccb8
zack.zhang <343178362@qq.com>
2024-06-06 15:42:19 +0800

    add var

--- variables.tf
+++ variables.tf
@@ -1,4 +1,13 @@
+variable "access_key" {
+  type    = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type    = string
+  sensitive = true
+}
+
 variable "region" {
   type    = string
-  default = ""
 }
